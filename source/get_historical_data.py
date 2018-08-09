3b652877ff32c755ac57a37f2422db7a107c9a5f
Eric Meyer <MeyerEricF@JohnDeere.com>
2018-08-09 16:03:04 -0500

    Add plot_selected, and normalize_data.

--- source/get_historical_data.py
+++ source/get_historical_data.py
@@ -2,8 +2,15 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-TICKERS = ['AAPL', 'AMD']
+
+AAPLE = 'AAPL'
+AMD = 'AMD'
+GOLD = 'GLD'
+GOOGLE = 'GOOGL'
+IBM = 'IBM'
 SP500 = 'SPY'
+
+SYMBOLS = [AAPLE, AMD, GOLD, GOOGLE, IBM]
 
 
 def symbol_to_path(symbol, base_dir='data'):
@@ -29,7 +36,7 @@ def get_data(symbols, dates, columns=['Adj Close']):
     if SP500 not in symbols:
         symbols.insert(0, SP500)
 
-    for symbol in TICKERS:
+    for symbol in symbols:
         # Join the 2 dataframes
         df_tmp = get_data_from_csv(symbol, columns)\
             .rename(columns={'Adj Close': symbol})
@@ -74,37 +81,62 @@ def get_max_close(data):
     return data['Close'].max()
 
 
-def plot_data(symbol, data, columns):
+def plot_data(df, title='Stock prices'):
     """
-    Plots symbols.
-    :param symbol:
-    :param data:
-    :param columns:
+    Plots the data contained in the dataframe.
+    :param df: dataframe to plot
+    :param title: title of the plot chart
+    :return: nothing
+    """
+    ax = df.plot(title=title, fontsize=10)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Price')
+    plt.show()
+
+
+def plot_selected(df, columns, start_index, end_index):
+    """
+    Plots the selected columns over the selected date range.
+    :param df: dataframe containing stock prices
+    :param columns: columns to plot from the dataframe
+    :param start_index: start index
+    :param end_index: end index
+    :return: nothing
+    """
+    plot_data(df.ix[start_index:end_index, columns])
+
+
+def normalize_data(df):
+    """
+    Returns the normalized dataframe, by using the first row of the dataframe.
+    :param df:
     :return:
     """
-    data[columns].plot()
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title('{}'.format(symbol))
-    plt.show()
+    return df/df.ix[0]
 
 
 def test_run():
     # Define the date range
-    start_date = '2010-01-22'
-    end_date = '2010-01-26'
+    start_date = '2010-01-01'
+    end_date = '2010-12-31'
     dates = pd.date_range(start_date, end_date)
 
-    df = get_data(TICKERS, dates)
+    df = get_data(SYMBOLS, dates)
     print(df)
 
-    # columns = ['Close', 'High', 'Low', 'Volume']
-    # for symbol in TICKERS:
-    #     df = get_data_from_csv(symbol, columns)
-    #     print(symbol)
-    #     print('Max close = {}'.format(get_max_close(df)))
-    #     print('Mean volume = {}'.format(get_mean_volume(df)))
-    #     plot_data(symbol, df, columns)
+    # row slicing using DataFrame.ix[] selector
+    # print(df.ix[start_date:'2010-01-31'])
+
+    # slice by column (symbol)
+    # print(df['SPY'])
+    # print(df[['SPY', 'AAPL']])
+
+    # slice by row and column
+    # print(df.ix['2010-03-10':'2010-03-15', ['SPY', 'AMD']])
+
+    plot_data(df)
+    plot_selected(df, [SP500, GOOGLE, IBM, GOLD], start_date, end_date)
+    plot_data(normalize_data(df))
 
 
 if __name__ == '__main__':
